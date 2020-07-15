027a52f333853d77fec0535a3f6038663288ab65
shichen.hong <shichen.hong@invensity.com>
2020-07-16 01:05:23 +0200

    Added static lib linking in cmakeLists

--- myGame/main.cpp
+++ myGame/main.cpp
@@ -4,23 +4,56 @@
 int main(int argc, char* argv[])
 {
     std::vector<sf::VideoMode> window_modes = sf::VideoMode::getFullscreenModes();
+    
     sf::Uint32 style = sf::Style::Default;
     // sf::Uint32 style = sf::Style::Fullscreen;
-    sf::Window window(sf::VideoMode(400, 300), "My Game", style);
-    window.setMouseCursorVisible(false);
-
-
+    sf::RenderWindow window(sf::VideoMode(400, 300), "My Game", style);
+    changeWindowColor(window, sf::Color::Black);
+    
     while(window.isOpen())
     {
         sf::Event event_1;
-        while (window.pollEvent(event_1))
-        {
-
-        }
+        basicEvent(window, event_1);
     }
-
     sf::sleep(sf::seconds(5));
     return 0;
 }
 
+void basicEvent(sf::RenderWindow& window, sf::Event& event)
+{
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::EventType::Closed:
+                window.close();
+                break;
+            case sf::Event::EventType::KeyReleased:
+                if (event.key.code == sf::Keyboard::Space)
+                {
+                    window.setMouseCursorVisible(true);
+                    changeWindowColor(window, sf::Color::Blue);
+                }
+                else if (event.key.code == sf::Keyboard::BackSpace)
+                {
+                    window.setMouseCursorVisible(false);
+                    changeWindowColor(window, sf::Color::Black);
+                }
+                break;
+            case sf::Event::LostFocus:
+                window.setTitle("Lost Focus");
+                break;
+            case sf::Event::GainedFocus:
+                window.setTitle("My Game");
+                break;
+            default:
+                break;
+            }
+        }
+}
 
+void changeWindowColor(sf::RenderWindow& window, sf::Color color)
+{
+    window.clear(color);
+    window.display();
+}
--- myGame/main.hpp
+++ myGame/main.hpp
@@ -2,8 +2,11 @@
 #define __main
 
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <vector>
 
 void setWindow();
+void basicEvent(sf::RenderWindow &window, sf::Event &event);
+void changeWindowColor(sf::RenderWindow &window, sf::Color color);
 
 #endif
